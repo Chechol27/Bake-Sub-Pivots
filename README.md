91198ad94d129b53b6562204b819bc1d3d0143d1
chechoEducativo <156550492+chechoEducativo@users.noreply.github.com>
2024-08-27 20:55:54 -0500

    organized functions for different bake types

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ A tool to bake sub pivots in a multi-element mesh, helping with draw call optimi
 
 
 ![Sub pivots baker](https://github.com/user-attachments/assets/4fb3ecce-793d-4c3c-8429-2b97d941cce5)
+
+
--- bake_sub_pivots.py
+++ bake_sub_pivots.py
@@ -8,22 +8,37 @@ class BakeSubPivots(bpy.types.Operator):
     bl_label = "Bake Sub Pivots"
     bl_options = {'UNDO'}
 
+    @staticmethod
+    def calculate_pivot_from_vertex_indices(context: bpy.types.Context, obj: bpy.types.Object) -> (Vector, Vector):
+        scene = context.scene
+        settings = scene.bake_sub_pivot_settings
+        origin_vertex_indices = [settings.BSP_origin_vertex_indices_0, settings.BSP_origin_vertex_indices_1]
+        normal_vertex_indices = [settings.BSP_normal_vertex_index]
+        new_pivot: Vector = Vector((0, 0, 0))
+        normal: Vector = Vector((0, 0, 0))
+        for v_id in origin_vertex_indices:
+            new_pivot += obj.data.vertices[v_id].co / len(origin_vertex_indices)
+        for v_id in normal_vertex_indices:
+            normal += obj.data.vertices[v_id].normal / len(normal_vertex_indices)
+        return new_pivot, normal
 
-    def execute(self, context):
+    @staticmethod
+    def calculate_pivot_from_object_pivot(obj: bpy.types.Object) -> (Vector, Vector):
+        new_pivot = obj.matrix_world.to_translation().xzy
+        normal = Vector((0, 0, 0))
+        all_normals = [v.normal for v in obj.data.vertices]
+        for n in all_normals:
+            normal += n / len(all_normals)
+        return new_pivot, normal.xzy
+
+    def execute(self, context: bpy.types.Context):
         selected_objects = context.selected_objects
-        origin_vertex_indices = [3,4]
-        normal_vertex_indices = [1]
-        tangent_vertex_indices = [3,4]
+        baking_mode = context.scene.bake_sub_pivot_settings.BSP_bake_mode
         for obj in selected_objects:
-            bpy.ops.object.origin_set(type="ORIGIN_CURSOR")
-            new_pivot: Vector
-            normal: Vector
-            new_pivot = Vector((0, 0, 0))
-            for v_id in origin_vertex_indices:
-                new_pivot += obj.data.vertices[v_id].co / len(origin_vertex_indices)
-            normal = Vector((0, 0, 0))
-            for v_id in normal_vertex_indices:
-                normal += obj.data.vertices[v_id].normal / len(normal_vertex_indices)
+            if baking_mode == "VERTICES_MAJOR":
+                new_pivot, normal = BakeSubPivots.calculate_pivot_from_vertex_indices(context, obj)
+            else:
+                new_pivot, normal = BakeSubPivots.calculate_pivot_from_object_pivot(obj)
 
             print(f"Sub Pivot: {new_pivot}")
             print(f"Rotation Axis: {normal}")
@@ -35,6 +50,7 @@ class BakeSubPivots(bpy.types.Operator):
                 pos_xy.data[loop.index].uv = new_pivot.xz
                 pos_z_n_x.data[loop.index].uv = (new_pivot.y, normal.x)
                 n_yz.data[loop.index].uv = normal.zy
+        bpy.ops.object.origin_set(type="ORIGIN_CURSOR")
         bpy.ops.object.join()
         return {'FINISHED'}
 
